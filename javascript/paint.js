6347822ccdf97d7ad2a02feaa9ed1ebdffa0b77a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-09-28 19:52:55 -0300

    add erase and draw mode add pencil and eraser custom cursor

--- javascript/paint.js
+++ javascript/paint.js
@@ -1,10 +1,13 @@
 class Paint {
   constructor(id) {
     this.canvas = document.querySelector(`#${id}`);
+    this.btnDraw = document.querySelector('#btnDraw');
+    this.btnErase = document.querySelector('#btnErase');
     this.ctx = this.canvas.getContext('2d');
     this.ctx.strokeStyle = "#222222";
     this.ctx.lineWith = 2;
     this.drawing = false;
+    this.toolType = 'draw';
     this.mousePosition = { x: 0, y: 0 };
     this.lastPosition = this.mousePosition;
     this.canvas.addEventListener("mousedown", this.handlerMouseDown.bind(this));
@@ -15,6 +18,8 @@ class Paint {
     this.canvas.addEventListener("touchend", this.handlerTouchEnd.bind(this));
     this.canvas.addEventListener("touchmove", this.handlerTouchMove.bind(this));
     this.canvas.addEventListener("touchmove", this.draw.bind(this));
+    this.btnDraw.addEventListener('click', this.handlerToogleTool.bind(this));
+    this.btnErase.addEventListener('click', this.handlerToogleTool.bind(this));
   }
 
   init(width, height) {
@@ -81,12 +86,31 @@ class Paint {
 
   draw() {
     if (this.drawing) {
+      this.ctx.beginPath();
+      if (this.toolType === 'draw') {
+        this.ctx.globalCompositeOperation = 'source-over';
+        this.ctx.lineWidth = 2;
+      } else {
+        this.ctx.globalCompositeOperation = 'destination-out';
+        this.ctx.lineWidth = 5;
+      }
 
       this.ctx.moveTo(this.lastPosition.x, this.lastPosition.y);
       this.ctx.lineTo(this.mousePosition.x, this.mousePosition.y);
+      this.ctx.lineJoin = this.ctx.lineCap = 'round';
       this.ctx.stroke();
-      this.lastPosition = this.mousePosition;
     }
+    this.lastPosition = this.mousePosition;
+  }
+
+  handlerToogleTool(e) {
+    const tool = e.target.getAttribute('id') == 'btnDraw' ? 'draw' : 'erase';
+    this.setTool(tool);
+  }
+
+  setTool(tool) {
+    this.canvas.classList.toggle('eraser');
+    this.toolType = tool;
   }
 }
 
