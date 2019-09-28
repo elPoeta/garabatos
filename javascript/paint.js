df448199e4c733b5271e2e8579ca14afa051f4e1
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-09-28 16:23:30 -0300

    add draw event

--- javascript/paint.js
+++ javascript/paint.js
@@ -2,23 +2,34 @@ class Paint {
   constructor(id) {
     this.canvas = document.querySelector(`#${id}`);
     this.ctx = this.canvas.getContext('2d');
+    this.ctx.strokeStyle = "#222222";
+    this.ctx.lineWith = 2;
     this.drawing = false;
     this.mousePosition = { x: 0, y: 0 };
     this.lastPosition = this.mousePosition;
     this.canvas.addEventListener("mousedown", this.handlerMouseDown.bind(this));
+    this.canvas.addEventListener("mouseup", this.handlerMouseUp.bind(this));
+    this.canvas.addEventListener("mousemove", this.handlerMouseMove.bind(this));
+    this.canvas.addEventListener("mousemove", this.draw.bind(this));
   }
 
-  init(w, h) {
-    this.canvas.width = w;
-    this.canvas.height = h;
+  init(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
     return this;
   }
 
   handlerMouseDown(e) {
-    console.log(this);
     this.drawing = true;
     this.lastPosition = this.getMousePosition(e);
-    console.log('lastpos ', this.lastPosition);
+  }
+
+  handlerMouseUp(e) {
+    this.drawing = false;
+  }
+
+  handlerMouseMove(e) {
+    this.mousePosition = this.getMousePosition(e);
   }
 
   getMousePosition(e) {
@@ -28,5 +39,15 @@ class Paint {
       y: e.clientY - clientRect.top
     };
   }
+
+  draw() {
+    if (this.drawing) {
+      this.ctx.moveTo(this.lastPosition.x, this.lastPosition.y);
+      this.ctx.lineTo(this.mousePosition.x, this.mousePosition.y);
+      this.ctx.stroke();
+      this.lastPosition = this.mousePosition;
+    }
+  }
 }
+
 new Paint('canvas').init(720, 400);
